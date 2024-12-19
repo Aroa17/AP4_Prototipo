03217efa6bc72c2542d72dd1f285cb08ca934c59
Aroa17 <arhefr@alumnatflorida.es>
2024-12-19 03:09:13 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,20 @@ router.get("/rutaGetRouter", function(req, res) {
     res.send("Bienvenido a la web en la que estamos utilizando router desde el servidor");
 });
 
+router.get("/rutaGR2", function(req, res) {
+    res.send("Se le presenta al usuario el poder contemplar desde la web lo que el servidor hace");
+});
+
+router.get("/rutaGR3", function(req, res) {
+    res.send("La app a la que desea acceder se llama StickCar");
+});
+
+router.get("/rutaGR4", function(req, res) {
+    res.send("Todavía está en proceso de ejecución...");
+});
+
+router.get("/rutaGR5", function(req, res) {
+    res.send("Desde la ruta cinco, dejo este mensaje en la web desde el servidor");
+});
+
 module.exports = router;
