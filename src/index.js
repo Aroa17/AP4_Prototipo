a426c54a686b4471a5e61bf5f3c99068d61d6aa3
Aroa17 <arhefr@alumnatflorida.es>
2025-02-08 16:14:37 +0100

    pruebas POST

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.set("port", process.env.PORT || 8080);
 
 //rutas
 app.use(require("./routes/index"));
-
+ app-use(morgan("dev"));
 
 //iniciando el servidor
 app.listen(app.get("port"), function() {
