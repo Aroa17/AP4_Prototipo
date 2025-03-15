83ab3d9bbf5036f1370437fbee02ac8c64723c5f
Aroa17 <arhefr@alumnatflorida.es>
2025-03-15 23:21:31 +0100

    npm run start

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
-app.set("port", process.env.PORT || 8080);
 
-// middleware
+const path = require("path");
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
 
-//rutas
-app.use(require("./routes/index"));
-
-
-//iniciando el servidor
-app.listen(app.get("port"), function() {
-    console.log("Hola desde el puerto " + app.get("port"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "routes", "formulario.html"));
 });
 
+app.use(require("./routes/index"));
 
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+    console.log("Hola desde el puerto " + app.get("port"));
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,60 +1,73 @@
 const { Router } = require("express");
 const router = Router();
 
-//ruta Get
-
-router.get("/rutaGetRouter", function(req, res) {
+// Rutas GET
+router.get("/rutaGetRouter", function (req, res) {
     res.send("Bienvenido a la web en la que estamos utilizando router desde el servidor");
 });
 
-router.get("/rutaGR2", function(req, res) {
+router.get("/rutaGR2", function (req, res) {
     res.send("Se le presenta al usuario el poder contemplar desde la web lo que el servidor hace");
 });
 
-router.get("/rutaGR3", function(req, res) {
+router.get("/rutaGR3", function (req, res) {
     res.send("La app a la que desea acceder se llama StickCar");
 });
 
-router.get("/rutaGR4", function(req, res) {
+router.get("/rutaGR4", function (req, res) {
     res.send("Todavía está en proceso de ejecución...");
 });
 
-router.get("/rutaGR5", function(req, res) {
+router.get("/rutaGR5", function (req, res) {
     res.send("Desde la ruta cinco, dejo este mensaje en la web desde el servidor");
 });
 
-router.post("/rutaPOST", (req, res) => {
+// Ruta POST para el formulario
+router.post("/rutaPOST", function (req, res) {
     console.log("Alguien ha accedido a la ruta POST");
-    res.send("Haciendo pruebas con la ruta POST");
+    console.log(req.body.nombre);
+    console.log(req.body.apellidos);
+    console.log(req.body.email);
+    console.log(req.body.fechadenacimiento);
+    console.log(req.body.contraseña);
+    console.log(req.body.repetircontraseña);
+
+    res.json({
+        id: "001",
+        nombre: req.body.nombre,
+        apellidos: req.body.apellidos,
+        email: req.body.email,
+        fechadenacimiento: req.body.fechadenacimiento,
+        contraseña: req.body.contraseña,
+        repetircontraseña: req.body.repetircontraseña,
+    });
+
 });
 
 router.post("/rutaPOSTconBody", (req, res) => {
-    req.body = "Probando con el request";
-    console.log(req.body);
+    console.log("Probando con el request");
     res.json({
         saludo: "Hola desde el JSON",
     });
 });
 
 router.post("/rutaPOST2conBody", (req, res) => {
-    req.body = "Hola, usando el request";
-    console.log(req.body);
+    console.log("Hola, usando el request");
     res.json({
         Title: "Creacion de nueva ruta JSON",
     });
 });
 
 router.post("/rutaPOST3", (req, res) => {
-    req.body = "Hola de nuevo";
-    console.log(req.body);
+    console.log("Hola de nuevo");
     res.json({
-        Trabajo: "Proyecto Intermodular, probando con rutas POST"
-    })
+        Trabajo: "Proyecto Intermodular, probando con rutas POST",
+    });
 });
 
 router.post("/rutaPOSTult", (req, res) => {
     console.log("Alguien ha accedido a la ruta POST");
     res.send("Haciendo pruebas con POSTMAN");
 });
-   
+
 module.exports = router;
